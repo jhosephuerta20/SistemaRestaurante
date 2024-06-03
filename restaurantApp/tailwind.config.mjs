505a9ac7369dfce409f7a7832b49e1177809bc40
Jhosep <jhosephuerta206@gmail.com>
2024-06-03 15:37:02 -0500

    primer commit

---
+++ restaurantApp/tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				customDark: '#070200',
+				customYellow: '#FFB605',
+				customGrey: '#D9D9D9',
+				customOrange:'#FB5C02',
+			  },
+			  fontFamily: {
+				  jockey: ['JockeyOne', 'sans-serif'],
+				},
+				backgroundImage: {
+				  'login': "url('/src/assets/login.jpg')",
+				  'footer': "url('/src/assets/foote.jpg')",
+				}
+		},
+	},
+	plugins: [],
+}
